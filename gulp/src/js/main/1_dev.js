e290e37870f622eabf87c1c5947dd06478dd74a0
Adrian <adrian.snopek@o2.pl>
2019-01-08 12:10:35 +0100

    Rose Hill #2

--- gulp/src/js/main/1_dev.js
+++ gulp/src/js/main/1_dev.js
@@ -1,141 +1,143 @@
-
-var winCalc, sectionStep;
+var sectionStep;
 var topx = $(document).scrollTop();
 
-$(document).ready(function () {
-    sectionStep = 1;
+/* stepping */
 
+/* standard stepper */
+function stepStandard() {
+    $("[data-step='" + sectionStep + "']").toggleClass("hide");
+}
+
+/* loading tower */
+function stepLoading() {
+
+    /* set speed */
+    let towerSpeed = topx * 1;
+
+    /* set transitions of fade in and fade out of sections */
+    $(".section-form").css("transition-duration", towerSpeed + "ms");
+    $(".section-loading").css("transition-duration", towerSpeed * 0.8 + "ms");
+
+    /* minutes and seconds calc */
+    let minutes = parseInt(topx / 60);
+    let decimalsMin = parseInt(minutes / 10);
+    let singleMin = minutes - decimalsMin * 10;
+    let seconds = parseInt(topx - (minutes * 60));
+    let decimalsSec = parseInt(seconds / 10);
+    let singleSec = seconds - decimalsSec * 10;
+    $("[data-invitation='0']").addClass("hide");
+    $("[data-invitation='1']").removeClass("hide");
+
+    let lineHeight = -22;
+
+    $("[data-decimalsMin]").css("top", lineHeight * decimalsMin);
+    $("[data-singleMin]").css("top", lineHeight * singleMin);
+    $("[data-decimalsSec]").css("top", lineHeight * decimalsSec);
+    $("[data-singleSec]").css("top", lineHeight * singleSec);
+
+    /* animation */
+    setTimeout(function () {
+        $("[data-step='" + sectionStep + "']").toggleClass("hide");
+        $('html, body').animate({
+            scrollTop: $($("#body")).offset().top
+        }, towerSpeed);
+
+        $(".counter-load span:not(.data-divider)").css('top', "0px").css("transition-duration", towerSpeed + "ms");
+
+    }, 1000);
+}
+
+function scrollBottom() {
     $('html,body').animate({ scrollTop: document.body.scrollHeight }, "0");
+}
 
-    /* */
+$(document).ready(function () {
     /* */
     /* onready events start */
     /* */
-    /* */
+
+    /* start section */
+    sectionStep = 1;
+
+    scrollBottom();
 
 
+    /* form sent? */
+    ((sessionStorage.getItem('submit') == 1) ? $("[data-step='4']").removeClass("hide") : $("[data-step='1']").removeClass("hide"));
 
-
-    /* */
     /* */
     /* onready events end */
     /* */
-    /* */
 
-    /* */
-    /* */
-    /* onmove events start */
-    /* */
-    /* */
-
-
-
-    /* */
-    /* */
-    /* onmove events end */
-    /* */
-    /* */
-
-
-    /* */
     /* */
     /* onclick events start */
     /* */
-    /* */
 
-
-
-    /* fold, expand text */
+    /* expand class */
     $("[data-expand]").click(function () {
         var item = $(this).attr("data-expand");
         $("[data-expandToggle='" + item + "']").toggleClass("toggleNotActive");
     });
 
-    /* stepping */
-    function stepStandard() {
-        $("[data-step='" + sectionStep + "']").toggleClass("hide");
-    }
-
-    /* loading tower */
-    function stepLoading() {
-
-        /* set speed */
-        let towerSpeed = topx * 4;
-
-        /* set transitions of fade in and fade out of sections */
-        $(".section-form").css("transition-duration", towerSpeed + "ms");
-        $(".section-loading").css("transition-duration", towerSpeed * 0.8 + "ms");
-
-        /* minutes and seconds calc */
-        let minutes = parseInt(topx / 60);
-        let decimalsMin = parseInt(minutes / 10);
-        let singleMin = minutes - decimalsMin * 10;
-        let seconds = parseInt(topx - (minutes * 60));
-        let decimalsSec = parseInt(seconds / 10);
-        let singleSec = seconds - decimalsSec * 10;
-        $("[data-invitation='0']").addClass("hide");
-        $("[data-invitation='1']").removeClass("hide");
-
-        var lineHeight = -22;
-
-        $("[data-decimalsMin]").css("top", lineHeight * decimalsMin);
-        $("[data-singleMin]").css("top", lineHeight * singleMin);
-        $("[data-decimalsSec]").css("top", lineHeight * decimalsSec);
-        $("[data-singleSec]").css("top", lineHeight * singleSec);
-
-        /* animation */
-        setTimeout(function () {
-            $("[data-step='" + sectionStep + "']").toggleClass("hide");
-            $('html, body').animate({
-                scrollTop: $($("#body")).offset().top
-            }, towerSpeed);
-
-            $(".counter-load span:not(.data-divider)").css('top', "0px").css("transition-duration", towerSpeed + "ms");
-
-        }, 1000);
 
 
-
-
-
-    }
-
-    $("[data-goStep]").click(function () {
+    $("[data-gostep]").click(function () {
         ((sectionStep == 2) ? "" : $("[data-step='" + sectionStep + "']").toggleClass("hide"));
-        sectionStep = $(this).attr("data-goStep");
+        sectionStep = $(this).attr("data-gostep");
         ((sectionStep == 3) ? $(".body").removeClass('stop-scroll') : $(".body").addClass('stop-scroll'));
         ((sectionStep == 3) ? stepLoading() : stepStandard());
+        ((sectionStep == 1) ? $("[data-step='" + sectionStep + "']").removeClass("hide") : "");
     });
 
-    /* */
+    /* send form */
+    $("[data-submit='1']").click(function () {
+        let submit = 0;
+/* 
+        $(".input-easy[aria-required='required']").each(function (index) {
+            let t = $(this);
+            let x = $(this).val();
+            
+            if (x == "") {
+                console.log(x);
+                t.closest(".error-block").html("This field cannot be empty.").removeClass("d-none");
+            }
+            else {
+                submit = 1;
+            }
+        });
+*/
+            sessionStorage.setItem('submit', '1');
+            $("#mainform").submit();
+    });
+
+    /* reset sending */
+    $("[data-submit='0']").click(function () {
+        sessionStorage.removeItem('submit');
+        $("[data-step='4']").addClass("hide");
+    });
+
     /* */
     /* onclick events end */
     /* */
-    /* */
 
-
-    /* */
     /* */
     /* onload events start */
     /* */
-    /* */
     window.addEventListener('load', function () {
         /* tooltip init */
-        $(function () {
-            $('[data-tooltips="tooltip"]').tooltip();
-        });
+        if ($(window).width() > 992) {
+            $(function () {
+                $('[data-tooltips="tooltip"]').tooltip();
+            });
+        }
     });
 
     /* */
-    /* */
     /* onload events end */
-    /* */
     /* */
 
     /* */
-    /* */
     /* onscroll events start */
-    /* */
     /* */
     var cached = null;
     window.addEventListener('scroll', function (event) {
@@ -148,16 +150,17 @@ $(document).ready(function () {
         cached = event;
     });
     /* */
+    /* onload events end */
+    /* */
+
+    /* */
+    /* onsresize events start */
+    /* */
+    $(window).resize(function () {
+        scrollBottom();
+    });
     /* */
     /* onload events end */
     /* */
-    /* */
-
 }
 );
-
-
-
-
-
-
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php include "include/header.php"?>
-<section class="section-start standard-transition pointer d-flex align-items-center justify-content-center vh-100 bg-blue1 absolute s-steps bot"
-  data-step="1" data-goStep="2">
-  <object class="p-none" type="image/svg+xml" data="images/static/logo.svg">
+<section class="section-start standard-transition pointer d-flex align-items-center justify-content-center vh-100 bg-blue1 absolute s-steps bot hide px-5"
+  data-step="1" data-gostep="2">
+  <object class="p-none img-fluid" type="image/svg+xml" data="images/static/logo.svg">
     Your browser does not support SVG
   </object>
 </section>
 <section class="section-loading hide s-steps " data-step="2">
   <div class="appel fixed w-100 text-right vh-100">
     <svg class="svg-i-volume svg-i-volume-dims pointer relative right-icos"></svg>
-    <div class="counter text-gray1 absolute pointer" data-goStep="3" data-scroll="body">
+    <div class="counter text-gray1 absolute pointer" data-gostep="3" data-scroll="body">
       <p class="counter-load" data-invitation="0">Invitation</p>
       <div class="counter-load hide" data-invitation="1">
         <span data-decimalsMin>0123456789</span>
@@ -28,75 +28,89 @@
 </section>
 <section class="section-form bg-blue1 text-gray1 absolute s-steps hide" data-step="3">
   <div class="container text-center">
-    <h2 class="pb-5">
+    <h2 class="pb-0 pb-xl-5">
       A new artistic vision <br>
       in residential real estate <br>
       built by Rockefeller Group <br>
       takes center stage on <br>
       New York City’s Rose Hill
     </h2>
-    <h3 class="pt-5 pb-5">
+    <h3 class="pt-5 pb-3 pb-xl-5">
       We invite you to receive more information as it becomes <br>
       available and prior to the commencement of sales this spring 2019.<br><br>
       Please send us your contact information
     </h3>
-    <form class="pt-5 mb-5 pb-3">
+    <form class="pt-5 mb-5 pb-3" id="mainform">
       <div class="form-group mb-5 pb-3">
-        <input class="input-easy" type="text" placeholder="FIRST NAME*">
+        <input class="input-easy" type="text" placeholder="FIRST NAME*" aria-required="required">
+        <p class="error-block d-none"></p>
       </div>
       <div class="form-group mb-5 pb-3">
-        <input class="input-easy" type="text" placeholder="LAST NAME*">
+        <input class="input-easy" type="text" placeholder="LAST NAME*" aria-required="required">
+        <p class="error-block d-none"></p>
       </div>
       <div class="form-group mb-5 pb-3">
         <input class="input-easy" type="text" placeholder="PHONE">
+        <p class="error-block d-none"></p>
       </div>
       <div class="form-group mb-5 pb-3">
-        <input class="input-easy" type="text" placeholder="EMAIL*">
+        <input class="input-easy" type="text" placeholder="EMAIL*" aria-required="required">
+        <p class="error-block d-none"></p>
       </div>
       <div class="form-group mb-5 pb-3">
         <input class="input-easy" type="text" placeholder="HOW DID YOU HEAR ABOUT US?">
+        <p class="error-block d-none"></p>
       </div>
       <div class="d-flex justify-content-center">
         <p>ARE YOU A BROKER?</p>
         <div class="form-group ml-4 mb-5">
-          <input type="radio" id="huey" name="drone" value="huey" checked>
-          <label for="huey">Huey</label>
+          <input type="radio" id="yes" name="broker" value="huey">
+          <label for="yes">Yes</label>
         </div>
         <div class="form-group ml-4">
-          <input type="radio" id="dewey" name="drone" value="dewey">
-          <label for="dewey">Dewey</label>
+          <input type="radio" id="no" name="broker" value="dewey">
+          <label for="no">No</label>
         </div>
+        <p class="error-block d-none"></p>
       </div>
-      <button class="button btn-white-blue" type="submit">SEND</button>
+      <!-- submit in js code + sessionstorage -->
+      <button class="button btn-white-blue" type="button" data-submit="1">
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+        <span>SEND</span>
+      </button>
     </form>
   </div>
   <div class="container-fluid">
-    <div class="row px-5">
-      <div class="col-12 col-md-8">
+    <div class="row">
+      <div class="col-12 col-md-8 d-none d-md-block">
         <p>
           30 East 29th Street, New York, NY 10016
         </p>
       </div>
-      <div class="col-12 col-md-4 text-right relative">
-        <div class="d-inline-flex align-items-center h-100 mr-5 pr-4">
-          <a href="#" class="d-inline-flex mr-2" target="_blank" data-tooltips="tooltip" data-placement="bottom" title="Go to our Twitter">
+      <div class="col-12 col-md-4 text-right relative d-flex flex-row-reverse justify-content-between d-md-block ">
+        <div class="links d-inline-flex align-items-center h-100 mr-md-5 pr-md-4">
+          <a href="#" title="Twitter" class="d-inline-flex mr-2" target="_blank" data-tooltips="tooltip" data-placement="bottom" title="Go to our Twitter">
             <svg class="svg-i-tw svg-i-tw-dims"></svg>
           </a>
-          <a href="#" class="d-inline-flex mr-2" target="_blank" data-tooltips="tooltip" data-placement="bottom" title="Go to our Instagram">
+          <a href="#" title="Instagram" class="d-inline-flex mr-2" target="_blank" data-tooltips="tooltip" data-placement="bottom" title="Go to our Instagram">
             <svg class="svg-i-ig svg-i-ig-dims"></svg>
           </a>
-          <a href="#" class="d-inline-flex" target="_blank" data-tooltips="tooltip" data-placement="bottom" title="Go to our Facebook">
+          <a href="#" title="Facebook" class="d-inline-flex" target="_blank" data-tooltips="tooltip" data-placement="bottom" title="Go to our Facebook">
             <svg class="svg-i-fb svg-i-fb-dims"></svg>
           </a>
         </div>
         <div class="d-inline-block">
           <p class="pointer" data-expand="1" data-tooltips="tooltip" data-placement="bottom" title="See more informations">LEGAL</p>
-          <div class="tooltip-source bg-gray1 absolute d-flex toggleNotActive" data-expandToggle="1" data-expand="1"
+          <div class="tooltip-source bg-gray1 d-flex align-items-center toggleNotActive  pointer" data-expandToggle="1" data-expand="1"
             data-tooltips="tooltip" data-placement="top" title="Close">
+            <svg class="svg-i-close-blue svg-i-close-blue-dims pointer d-md-none right-icos absolute"></svg>
             <div class="icon-container">
               <svg class="svg-i-holding svg-i-holding-dims"></svg>
             </div>
-            <div class="text text-justify text-container text-blue1">
+            <div class="text text-center text-md-justify text-container text-blue1">
               <p>
                 THE SPONSOR IS AP BLOCK 176 VENTURE URBAN RENEWAL, LLC, A
                 ROCKEFELLER GROUP, AND MAKES NO REPRESENTATION OR WARRANTIES
@@ -110,8 +124,8 @@
     </div>
   </div>
 </section>
-<section class="section-end standard-transition d-flex align-items-center justify-content-center vh-100 text-center bg-color1 s-steps absolute hide bot">
-  <svg class="svg-i-close-blue svg-i-close-blue-dims absolute pointer right-icos" data-resetAll></svg>
+<section class="section-end standard-transition d-flex align-items-center justify-content-center vh-100 text-center bg-color1 s-steps absolute hide bot" data-step="4">
+  <svg class="svg-i-close-blue svg-i-close-blue-dims absolute pointer right-icos" data-gostep="1" data-submit="0"></svg>
   <div class="text-blue1">
     <h2>
       Thank You
